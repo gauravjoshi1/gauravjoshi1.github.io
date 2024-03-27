e31bb634e60daba1fbdea6082d4fb8502ac6fba7
Gaurav Joshi <gaurav.joshi@utdallas.edu>
2024-03-26 21:06:05 -0500

    Brute Force to render light mode

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,4 +1,4 @@
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleDarkIcon = document.getElementById('theme-toggle-light-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
 // Change the icons inside the button based on previous settings
@@ -19,8 +19,8 @@ themeToggleBtn.addEventListener('click', function() {
     // if set via local storage previously
     if (localStorage.getItem('color-theme')) {
         if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
+            document.documentElement.classList.add('light');
+            localStorage.setItem('color-theme', 'light');
         } else {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
@@ -33,7 +33,7 @@ themeToggleBtn.addEventListener('click', function() {
             localStorage.setItem('color-theme', 'light');
         } else {
             document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
+            localStorage.setItem('color-theme', 'light');
         }
     }
     
