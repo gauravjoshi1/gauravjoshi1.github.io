294941ba337bb1f673188618aad8e302ed283377
Gaurav Joshi <gaurav.joshi@utdallas.edu>
2024-03-26 21:25:20 -0500

    Removed dark theme

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -16,25 +16,7 @@ themeToggleBtn.addEventListener('click', function() {
     themeToggleDarkIcon.classList.toggle('hidden');
     themeToggleLightIcon.classList.toggle('hidden');
 
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('light');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
-
-    // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
-    }
+    document.documentElement.classList.add('light');
+    localStorage.setItem('color-theme', 'light');
     
 });
